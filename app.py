3b890289d3fd4c34cdd79a0c733c43333854e08c
furoiusf <jackihui@gmail.com>
2025-10-26 18:06:34 +0000

    Initial commit - Habit Thankful Journal

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Habit Thankful Journal", page_icon="🪶")
+st.title("🪶 Habit Thankful Journal")
+st.write("Hello Gladius! 如果你見到呢行字，即係 Streamlit 環境 OK 🎉")
